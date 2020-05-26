a14bf40f25c17a631e1c4c70a22ae533a0b90d57
eusebio <eusebio.guijarro@protonmail.com>
2020-05-26 23:16:24 +0200

    controladores

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,7 +6,85 @@ $(function () {
 
 function startEvents() {
     startEventClearResult();
+    startSelectEvent();
+    startForm();
+}
+
+
+function startForm() {
     startFormAccounts();
+    startFormMap_entry();
+    startFormFees();
+    startFormContractInterface();
+    startFormContractSource();
+}
+
+function startFormContractSource() {
+    $("#contractsource").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: $('#contractsource').attr('method'),
+            url: "form/contracts/source/" + $("#server").val() +"/"+ $("#stacksaddress").val() +"/"+ $("#contractnamecontractsource").val(),
+            contentType: false,
+            processData: false,
+            cache: false,
+            success: function (data) {
+                $("#result").text(data);
+            },
+            error: function (data) {
+                $("#result").text(data);
+            }
+        });
+    });
+}
+
+function startFormContractInterface() {
+    $("#contractinterface").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: $('#contractinterface').attr('method'),
+            url: "form/contracts/interface/" + $("#serverinterface").val() +"/"+ $("#stacksaddress").val() +"/"+ $("#contractnamecontractinterface").val(),
+            contentType: false,
+            processData: false,
+            cache: false,
+            success: function (data) {
+                $("#result").text(data);
+            },
+            error: function (data) {
+                $("#result").text(data);
+            }
+        });
+    });
+}
+
+function startFormFees() {
+    $("#feesForm").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: $('#feesForm').attr('method'),
+            url: "form/fees/transfer/" + $("#serverfees").val(),
+            contentType: false,
+            processData: false,
+            cache: false,
+            success: function (data) {
+                $("#result").text(data);
+            },
+            error: function (data) {
+                $("#result").text(data);
+            }
+        });
+    });
+}
+
+function startSelectEvent() {
+    $("#selectquery").change(
+            function () {
+                $("form").each(function () {
+                    $(this).parent().hide();
+                });
+                var sel = $("select option:selected").val();
+                $("#form" + sel).show();
+            });
 
 }
 
@@ -21,11 +99,34 @@ function startFormAccounts() {
         e.preventDefault();
         $.ajax({
             type: $('#accountsForm').attr('method'),
-            url:  "form/accounts/"+$("#server").val()+"/"+$("#principal").val(),
+            url: "form/accounts/" + $("#server").val() + "/" + $("#principal").val(),
             contentType: false,
             processData: false,
             cache: false,
             success: function (data) {
+                $("#result").text(data);
+            },
+            error: function (data) {
+                $("#result").text(data);
+            }
+        });
+    });
+}
+
+function startFormMap_entry() {
+    $("#map_entryForm").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: $('#map_entryForm').attr('method'),
+            url: "form/map_entry/" + $("#servermap").val() + "/" +
+                    $("#stackaddress").val() + "/" + $("#contractname").val() + "/" + $("#mapname").val(),
+            contentType: false,
+            processData: false,
+            cache: false,
+            success: function (data) {
+                $("#result").text(data);
+            },
+            error: function (data) {
                 $("#result").text(data);
             }
         });
--- routes/form.js
+++ routes/form.js
@@ -1,17 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var dir="http://";
-var port=20443;
+var dir = "http://";
+var port = 20443;
 var request = require('sync-request');
 
 
+
+//GET /v2/accounts/[Principal]
+
 router.post('/accounts/:server/:principal', function (req, res, next) {
-    var principal=req.params["principal"];
-    var server=req.params["server"];
-    var ans= request('GET', dir+server+":"+port+'/v2/accounts/'+principal);
+    var principal = req.params["principal"];
+    var server = req.params["server"];
+    var ruta=dir + server + ":" + port + '/v2/accounts/' + principal;
+//    res.send(ruta);
+    var ans = request("GET",ruta);
     res.send(ans.getBody());
-    res.send("ssssssssssss");
 });
+
+
+router.post('/map_entry/:server/:stackAddress/:contractName/:mapName', function (req, res, next) {
+    var server = req.params["server"];
+    var stackAddress = req.params["stackAddress"];
+    var contractName = req.params["contractName"];
+    var mapName = req.params["mapName"];
+    var ans = request('POST', dir + server + ":" + port + '/v2/map_entry/' + stackAddress+"/"+contractName+"/"+mapName,
+    {json: {}});
+    res.send(ans.getBody());
+});
+
+router.post('/fees/transfer/:server', function (req, res, next) {
+    var server=req.params["server"];
+    var ans = request('GET', dir + server + ":" + port + '/v2/fees/transfer');
+    res.send(ans.getBody());
+});
+
+router.post('/contracts/interface/:server/:stacksAddress/:contractName', function (req, res, next) {
+    var server=req.params["server"];
+    var stacksAddress=req.params["stacksAddress"];
+    var contractName=req.params["contractName"];
+    var ans = request('GET', dir + server + ":" + port + '/v2/contracts/interface/'+'/'+stacksAddress+'/'+contractName);
+    res.send(ans.getBody());
+});
+
+
+router.post('/contracts/source/:server/:stacksAddress/:contractName', function (req, res, next) {
+    var server=req.params["server"];
+    var stacksAddress=req.params["stacksAddress"];
+    var contractName=req.params["contractName"];
+    var ans = request('GET', dir + server + ":" + port + '/v2/contracts/source/'+'/'+stacksAddress+'/'+contractName);
+    res.send(ans.getBody());
+});
+
+
+
 
 module.exports = router;
 
